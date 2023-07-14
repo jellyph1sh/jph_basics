a31195b6582ea58d9b2857a5f15e5b1cde5edd99
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-07-14 05:02:46 +0200

    :sparkles: :bug:| Add notifications and fix error condition.

--- Client/ClientMain.cs
+++ Client/ClientMain.cs
@@ -8,14 +8,21 @@ namespace jph_basics.Client
 {
     public class ClientMain : BaseScript
     {
+        public void Notify(string text) {
+            BeginTextCommandThefeedPost("STRING");
+            AddTextComponentSubstringPlayerName(text);
+            EndTextCommandThefeedPostTicker(true, true);
+        }
         public bool GiveWeapon(int ped, string model, int ammo, bool inHand) {
             var hash = (uint) GetHashKey(model);
             if (GetWeapontypeModel(hash) == 0) {
                 Debug.WriteLine("ERROR! Invalid weapon model");
+                Notify(("~r~ERROR! Invalid weapon model"));
                 return false;
             }
             if (ammo < 0) {
                 Debug.WriteLine("ERROR! Ammo amount can't be lower than 0!");
+                Notify(("~r~ERROR! Ammo amount can't be lower than 0!"));
                 return false;
             }
             GiveWeaponToPed(ped, hash, ammo, false, inHand);
@@ -26,6 +33,7 @@ namespace jph_basics.Client
         public void GiveWeaponCommand(int src, List<object> args, string rawCommand) {
             if (args.Count < 3 || args.Count > 3)  {
                 Debug.WriteLine("ERROR! Missing arguments (player, weapon model, ammo count)!");
+                Notify("~r~ERROR! Missing arguments (player, weapon model, ammo count)!");
                 return;
             }
             var targetSrc = src;
@@ -33,18 +41,21 @@ namespace jph_basics.Client
             try {
                 if (args[0].ToString() != "me") {
                     targetSrc = Int32.Parse(args[0].ToString());
+                    if (!NetworkIsPlayerActive(GetPlayerFromServerId(targetSrc))) {
+                        Debug.WriteLine("ERROR! Unknow player!");
+                        Notify(("~r~ERROR! Unknow player!"));
+                        return;
+                    }
                 }
                 ammoCount = Int32.Parse(args[2].ToString());
             } catch (FormatException) {
                 Debug.WriteLine("ERROR! Unable to convert args into integer!");
-                return;
-            }
-            if (!NetworkIsPlayerActive(GetPlayerFromServerId(targetSrc))) {
-                Debug.WriteLine("ERROR! Unknow player!");
+                Notify(("~r~ERROR! Unable to convert args into integer!"));
                 return;
             }
             if (GiveWeapon(GetPlayerPed(GetPlayerFromServerId(targetSrc)), args[1].ToString(), ammoCount, true)) {
                 Debug.WriteLine($"Successfully give {args[1]} to player {args[0]}!");
+                Notify(($"~g~Successfully give {args[1]} to player {args[0]}!"));
             }
         }
     }
